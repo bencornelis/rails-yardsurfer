157a06cf07d371141bb3765bb8b81f333d7485cd
Ben Cornelis & Michael Yanchar <student@epicodus.com>
2015-07-09 09:32:59 -0700

    let user favorite a listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,8 @@ class Listing < ActiveRecord::Base
   validates :name, :presence => true
   validates :location, :presence => true
   has_many :reviews
-  has_many :fans, through: :favorites, class_name: "User"
+  has_many :favorites
+  has_many :fans, through: :favorites, source: :user
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :listings
-  has_many :favorite_listings, through: :favorites, class_name: "Listing"
+  has_many :favorites
+  has_many :favorite_listings, through: :favorites, source: :listing
   has_many :reviews
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+
+  def favorited?(listing)
+    Favorite.exists?(user_id: self.id, listing_id: listing.id)
+  end
 end
